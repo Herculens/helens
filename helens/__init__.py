a66ab0065b60f1a5a1cc80a7e1ec64b4b80a646b
Aymeric Galan <aymeric.galan@gmail.com>
2026-04-09 11:43:58 +0200

    Ready for pypi

--- helens/__init__.py
+++ helens/__init__.py
@@ -1,4 +1,11 @@
 # Copyright (c) 2023, herculens developers and contributors
 # Copyright (c) 2024, helens developers and contributors
 
+from importlib.metadata import version, PackageNotFoundError
+
 from .solver import LensEquationSolver
+
+try:
+    __version__ = version("helens")
+except PackageNotFoundError:
+    __version__ = "unknown"
